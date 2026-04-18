4cb75ba4fc991fba3fad30eca51713ddbc29eaca
Sania <blogoesman@gmail.com>
2026-04-18 23:29:08 +0700

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,19 @@
+import dayjs from 'dayjs'
 
+export function generateInstallments(startDate: string, dayOfMonth: number, tenor: number, amount: number) {
+  const arr = []
+  let base = dayjs(startDate)
+
+  for (let i = 0; i < tenor; i++) {
+    arr.push({
+      due_date: base.add(i, 'month').date(dayOfMonth).toISOString(),
+      amount
+    })
+  }
+
+  return arr
+}
+
+export function diffDays(date: string) {
+  return dayjs(date).diff(dayjs(), 'day')
+}
